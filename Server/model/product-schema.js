55c412d2d5e589cf848518e6c22bdf3f64fab812
vinoddotcom <vy638814@gmail.com>
2022-06-19 12:50:32 +0530

    minor changes

--- Server/model/product-schema.js
+++ Server/model/product-schema.js
@@ -1,17 +1,16 @@
 import mongoose from 'mongoose';
 
 const productschema = new mongoose.Schema({
-    id:{
-   type: String,
-   required: true,
-   unique: true,
-    },
-    imageUrl: Object,
-    title: Object,
-    price: String,
-    description: Object,
-}
-);
+  id: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  imageUrl: Object,
+  title: Object,
+  price: String,
+  description: Object,
+});
 
 const Product =  mongoose.model("product", productschema);
 
