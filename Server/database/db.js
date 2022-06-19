aba0496abfe9d92c058cea6fc307b7c0c0740827
vinoddotcom <vy638814@gmail.com>
2022-06-19 12:27:27 +0530

    Removed user name and password

--- Server/database/db.js
+++ Server/database/db.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 
  const Connection = async (userName, password) => {
-    const URL= `mongodb://vinod:vy122427@ac-r29krhk-shard-00-00.dotfc5d.mongodb.net:27017,ac-r29krhk-shard-00-01.dotfc5d.mongodb.net:27017,ac-r29krhk-shard-00-02.dotfc5d.mongodb.net:27017/?ssl=true&replicaSet=atlas-6j0qv3-shard-0&authSource=admin&retryWrites=true&w=majority`
+    const URL= `mongodb://${userName}:${password}@ac-r29krhk-shard-00-00.dotfc5d.mongodb.net:27017,ac-r29krhk-shard-00-01.dotfc5d.mongodb.net:27017,ac-r29krhk-shard-00-02.dotfc5d.mongodb.net:27017/?ssl=true&replicaSet=atlas-6j0qv3-shard-0&authSource=admin&retryWrites=true&w=majority`
     try {
       await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true});
       console.log("Connection established")
